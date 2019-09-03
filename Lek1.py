fbf002133ca69a04d1c8e598fd414fff2f91c5af
Alexander Häggström <alexander.haggstrom@learnet.se>
2019-09-03 10:08:34 +0200

    först 80 minuterna op tisdag 03/09

--- Lek1.py
+++ Lek1.py
@@ -1,1 +1,10 @@
-print("Hej på er i ee17")
+print('hello world')
+
+firstName = "Alexander"
+lastName = "Häggström"
+dateOfBirth = 20011116
+age = 17
+teacher = True
+
+print(firstName , lastName) , str(age) + str(teacher))
+print(dateOfBirth)
