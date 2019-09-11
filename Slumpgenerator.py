e34e4df529ce62b867ce31375b1e4208d852f98a
Alexander Häggström <alexander.haggstrom@learnet.se>
2019-09-11 13:19:39 +0200

    2019-0-11

---
+++ Slumpgenerator.py
@@ -0,0 +1,17 @@
+import random
+
+print("vill du rulla en tärning")
+
+try:
+    sides = int(input("Hur många sidor: "))
+except:
+    print("Tärningen behöver 1+ sidor")
+    sides = 1
+
+run = "y"
+
+while run.lower() == "y":
+    randomNumber = random.randint(1, sides)
+    print(randomNumber)
+
+    run = input("Vill du Alexander rulla en till tärning Y/N")
